fa6ca69031462b2bf1b7e34b4f2c533da5676fc2
ThatGravyBoat <thatgravyboat@gmail.com>
2022-12-18 07:07:02 -0330

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("maven-publish")
     id("java")
 }
 
@@ -16,6 +17,47 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
+java {
+    withSourcesJar()
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set("Yet Another Binary Notation")
+                description.set("Resourceful Bees is a mod that adds bees to Minecraft.")
+                url.set("https://github.com/Team-Resourceful/YetAnotherBinaryNotation")
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("git:https://github.com/Team-Resourceful/YetAnotherBinaryNotation.git")
+                    developerConnection.set("git:https://github.com/Team-Resourceful/YetAnotherBinaryNotation.git")
+                    url.set("https://github.com/Team-Resourceful/YetAnotherBinaryNotation")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            setUrl("https://maven.resourcefulbees.com/repository/maven-releases/")
+
+            credentials {
+                username = System.getenv("MAVEN_USER")
+                password = System.getenv("MAVEN_PASS")
+            }
+        }
+    }
 }
